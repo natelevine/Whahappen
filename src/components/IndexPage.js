9ed43f8df7f08fd82d53f9456ac4204b9c53b077
christopherdubs <christopher@lendup.com>
2017-06-08 15:27:11 -0700

    super basic html & css for a timeline event

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 export const IndexPage = () => (
   <div className="home">
-    IndexPage
+
+    <table>
+      <tbody>
+      <tr>
+        <th>June 8</th>
+        <td className="time">12:31pm</td>
+        <td className="description">
+          <h4>Something happened</h4>
+          <p>On the site, here's some metadata about the event and it's probably really long and verbose so let's be real about that</p>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
   </div>
 );
 
