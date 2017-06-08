05e8b2500ec36050013cb9095a04a6bc109c764c
Nate Levine <nate.levine@lendup.com>
2017-06-08 15:07:53 -0700

    add search form

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import SearchForm from './SearchForm';
+
+const searchHandler = (val) => {
+  console.log("Handler triggered! val: " + val);
+}
 
 export const IndexPage = () => (
   <div className="home">
     IndexPage
+    <div>
+      <SearchForm searchHandler={searchHandler}/>
+    </div>
   </div>
 );
 
